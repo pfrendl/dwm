6a06ab4b112758ba81da9a210c039a9b7e89e9b1
pfrendl <pfre00@gmail.com>
2023-10-15 23:46:37 +0200

    Added dmenu_radio script hotkey

--- config.def.h
+++ config.def.h
@@ -88,6 +88,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_x,      spawn,          SHCMD("~/.scripts/dmenu_shutdown") },
+	{ MODKEY,                       XK_r,      spawn,          SHCMD("~/.scripts/dmenu_radio") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
